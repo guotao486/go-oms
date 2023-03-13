17bc0fdf4e4085a4b9857c3337827b38ae918e34
793954500@qq.com <793954500@qq.com>
2023-03-13 17:10:37 +0800

    feat: order menus

--- pkg/menus/menus.go
+++ pkg/menus/menus.go
@@ -1,7 +1,7 @@
 /*
  * @Author: GG
  * @Date: 2023-02-28 17:10:15
- * @LastEditTime: 2023-03-06 14:32:45
+ * @LastEditTime: 2023-03-10 09:15:46
  * @LastEditors: GG
  * @Description:
  * @FilePath: \oms\pkg\menus\menus.go
@@ -36,31 +36,82 @@ func GetMenus() []*Menus {
 					Title:     "新增用户",
 					Router:    "/user/create",
 					Sort:      1,
-					Role:      []uint8{1, 2},
+					Role:      []uint8{1},
 					ChildNode: nil,
 				},
 				{
 					Title:     "编辑用户",
 					Router:    "/user/update",
 					Sort:      1,
-					Role:      []uint8{1, 2},
+					Role:      []uint8{1},
+					ChildNode: nil,
+				},
+				{
+					Title:     "删除用户",
+					Router:    "/user/delete",
+					Sort:      1,
+					Role:      []uint8{1},
 					ChildNode: nil,
 				},
 			},
 		},
 		{
-			Title:     "用户组",
-			Router:    "/group",
-			Sort:      1,
-			Role:      []uint8{1, 2},
-			ChildNode: nil,
+			Title:  "用户组",
+			Router: "/group/",
+			Sort:   1,
+			Role:   []uint8{1, 2},
+			ChildNode: []*Menus{
+				{
+					Title:     "新增用户组",
+					Router:    "/group/create",
+					Sort:      1,
+					Role:      []uint8{1},
+					ChildNode: nil,
+				},
+				{
+					Title:     "编辑用户组",
+					Router:    "/group/update",
+					Sort:      1,
+					Role:      []uint8{1},
+					ChildNode: nil,
+				},
+				{
+					Title:     "删除用户组",
+					Router:    "/group/delete",
+					Sort:      1,
+					Role:      []uint8{1},
+					ChildNode: nil,
+				},
+			},
 		},
 		{
-			Title:     "订单管理",
-			Router:    "/order",
-			Sort:      0,
-			Role:      []uint8{1, 2},
-			ChildNode: nil,
+			Title:  "订单管理",
+			Router: "/order/",
+			Sort:   0,
+			Role:   []uint8{1, 2},
+			ChildNode: []*Menus{
+				{
+					Title:     "新增订单",
+					Router:    "/order/create",
+					Sort:      1,
+					Role:      []uint8{1},
+					ChildNode: nil,
+				},
+				{
+					Title:     "编辑订单",
+					Router:    "/order/update",
+					Sort:      1,
+					Role:      []uint8{1},
+					ChildNode: nil,
+				},
+				{
+					Title:     "删除订单",
+					Router:    "/order/delete",
+					Sort:      1,
+					Role:      []uint8{1},
+					ChildNode: nil,
+				},
+			},
 		},
 	}
 	return MenusTree
